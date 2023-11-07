fded4a1e9624c27526919b5c5249132446b8024a
keshav <keshavtayal002@gmail.com>
2023-11-07 15:24:58 +0530

    completed forgot-password

--- src/pages/ForgotPass.js
+++ src/pages/ForgotPass.js
@@ -1,61 +1,87 @@
-import React, { useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import "./ForgotPass.css";
 import mail_icon from "../assets/email.png";
 import { Link } from "react-router-dom";
-import { API_URL } from '../ConfigApi';
-
+import { API_URL } from "../ConfigApi";
+import Swal from 'sweetalert2';
+import HashLoader from "react-spinners/HashLoader";
+import ClipLoader from "react-spinners/ClipLoader";
+// import { BookLoader } from "react-awesome-loaders";
 function ForgotPass() {
-  const [email, setEmail] = useState("");
+  const override = {
+    marginLeft : "10px",
+  };
+  const [loading, setLoading] = useState(false);
+  
+  const [email, setEmail] = useState('');
   const navigate = useNavigate();
+
   const handlesendmail = () => {
-    // First, fetch the CSRF token
-    fetch(`${API_URL}/users/users/get_csrf_token/`, {
-      method: 'GET',
+    setLoading(true);
+    fetch(`${API_URL}/users/users/password_reset/`, {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
+      body: JSON.stringify({ email }),
     })
       .then((response) => response.json())
       .then((data) => {
-        const csrfToken = data.csrf_token;
-
-        // Now that you have the CSRF token, send the password reset request
-        fetch(`${API_URL}/users/users/password_reset/`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken, // Include the CSRF token in the headers
-          },
-
-          body: JSON.stringify({ email }),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.emailSent) {
-              navigate('/reset-password');
-            } else {
-              alert("Failed to send reset email. Please check your email address.");
-            }
-          })
-          .catch((error) => {
-            console.error('Error:', error);
-            alert("An error occurred. Please try again.");
+      setLoading(false);
+        if ('email' in data && data.email[0]) {
+          // Error response from the backend
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: data.email[0],
           });
+        } else if (data.status === 'OK') {
+          // Success response from the backend
+          Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: 'Password reset email sent successfully.',
+          });
+        } else {
+          // Unexpected response from the backend
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'An unexpected error occurred. Please try again.',
+          });
+        }
       })
       .catch((error) => {
-        console.error('Error fetching CSRF token:', error);
-        alert("An error occurred. Please try again.");
+      setLoading(false);
+        console.error('Error:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'An unexpected error occurred. Please try again.',
+        });
       });
   };
   return (
     <div className="forgot-password-page">
-    <div className="forgot-password-container">
-      <div className="forgot-password-header">Forgot Password</div>
-      <div className="forgot-password-instructions">
-        Enter your email address, and we'll send you instructions to reset your password.
-      </div>
-      <div className="forgot-password-input">
+       {/* {loading && (
+        <div className="overlay"></div>
+      )}
+       <HashLoader
+        color='#e3e3e3'
+        loading={loading}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      /> */}
+      <div className="forgot-password-container">
+        <div className="forgot-password-header">Forgot Password</div>
+        <div className="forgot-password-instructions">
+          Enter your email address, and we'll send you instructions to reset
+          your password.
+        </div>
+        <div className="forgot-password-input">
           <img src={mail_icon} alt="Email" />
           <input
             type="email"
@@ -63,16 +89,24 @@ function ForgotPass() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-      </div>
-      <button className="forgot-password-submit" onClick={handlesendmail}>
+        </div>
+        <button className="forgot-password-submit" onClick={handlesendmail}>
           Send Mail
+          <ClipLoader
+                  color='white'
+                  loading={loading}
+                  cssOverride={override}
+                  size={16}
+                  aria-label="Loading Spinner"
+                  data-testid="loader"
+                />
         </button>
-      <div className="forgot-password-login-link">
-        Remember your password?{' '}
-        <Link className="forgot-password-login-option" to="/login">
-          Log In
-        </Link>
-      </div>
+        <div className="forgot-password-login-link">
+          Remember your password?{" "}
+          <Link className="forgot-password-login-option" to="/login">
+            Log In
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,59 +1,173 @@
-import React, { useState } from "react";
-// import { useHistory } from 'react-router-dom';
-import "./ResetPassword.css"; // Include your custom CSS
+import React, { useEffect, useState } from "react";
+import "./ResetPassword.css"; 
 import pass_icon from "../assets/password.png";
+import { API_URL } from '../ConfigApi';
+import HashLoader from "react-spinners/HashLoader";
+import ClipLoader from "react-spinners/ClipLoader";
+import Swal from 'sweetalert2';
+import { useNavigate, useLocation, Link } from "react-router-dom";
 
 function ResetPassword() {
-  //   const history = useHistory();
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const override = {
+    marginLeft : "10px",
+  };
+  const [loading, setLoading] = useState(false);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [resetToken, setResetToken] = useState('');
+  const [tokenValid, setTokenValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleResetPassword = () => {
-    if (newPassword !== confirmPassword) {
-      setError("Passwords do not match.");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+    if (token) {
+      setResetToken(token);
     } else {
-      // You should send the new password and token to the server for validation.
-      // Implement the logic to send a reset request and redirect the user to a login page.
-      // This is a simplified example.
-      // ...
+      alert('Token is not present in the URL');
+      return;
+    }
+    fetchTokenValidity(token);
+  }, []);
+
+  const fetchTokenValidity = async (token) => {
+    try {
+      const response = await fetch(`${API_URL}/users/users/password_reset/validate_token/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token }),
+      });
+
+      if (response.status === 200) {
+        setTokenValid(true);
+      } else {
+        setTokenValid(false);
+        // Set the error message based on the token status
+        if (response.status === 404) {
+          setErrorMessage("It seems that you have already reset your password or the Reset password link has expired.");
+        } else {
+          setErrorMessage("An error occurred. Please try again.");
+        }
+      }
+    } catch (error) {
+      setTokenValid(false);
+      setErrorMessage("An error occurred. Please try again.");
+    }
+  };
+
+  const handleResetPassword = async () => {
+    try {
+      if (password !== confirmPassword) {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "Password and Confirm Password do not match. Please try again.",
+        });
+        return;
+      }
+      setLoading(true);
+      const response = await fetch(`${API_URL}/users/users/password_reset/confirm/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ password, token: resetToken }),
+      });
+      setLoading(false);
+      if (response.ok) {
+        navigate('/login');
+      } else {
+        const data = await response.json();
+        if (data.password) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Password reset failed',
+            text: data.password[0], 
+          });
+        } else {
+       
+          Swal.fire({
+            icon: 'error',
+            title: 'Password reset failed',
+            text: data.message, 
+          });
+      }
+    }
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "An error occurred. Please try again.",
+      });
     }
   };
 
   return (
     <div className="reset-password-page">
+      {/* {loading && (
+        <div className="overlay"></div>
+      )} */}
+      {/* <HashLoader
+        color='#e3e3e3'
+        loading={loading}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      /> */}
       <div className="reset-password-container">
         <div className="reset-password-box">
           <h2 className="reset-password-header">Reset Your Password</h2>
-          <p className="reset-password-instructions">
-            Choose a strong, secure password to protect your account.
-          </p>
-          {error && <div className="reset-password-error">{error}</div>}
-          <div className="reset-password-input">
-            <img src={pass_icon} alt="Email" />
-            <input
-              type="password"
-              placeholder="New Password"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-            />
+          {tokenValid ? (
+            <>
+              <p className="reset-password-instructions">
+                Choose a strong, secure password to protect your account.
+              </p>
+              <div className="reset-password-input">
+                <img src={pass_icon} alt="Email" />
+                <input
+                  type="password"
+                  placeholder="New Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="reset-password-input">
+                <img src={pass_icon} alt="Email" />
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+              <button className="reset-password-submit" onClick={handleResetPassword}>
+                Reset Password
+                <ClipLoader
+                  color='white'
+                  loading={loading}
+                  cssOverride={override}
+                  size={16}
+                  aria-label="Loading Spinner"
+                  data-testid="loader"
+                />
+              </button>
+            </>
+          ) : (
+            <>
+            <div className="alert alert-danger">{errorMessage}
+            </div>
+            <div className="signup-login-link">
+            Password already reset?{" "}
+            <Link className="signup-login-option" to="/login">
+              Login here
+            </Link>
           </div>
-          <div className="reset-password-input">
-            <img src={pass_icon} alt="Email" />
-
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </div>
-          <button
-            className="reset-password-submit"
-            onClick={handleResetPassword}
-          >
-            Reset Password
-          </button>
+            </>
+          )}
         </div>
       </div>
     </div>
